5e65b34700a6065a4536456839484aae99f23875
Britt Mo <brittany.dunne@utexas.edu>
2018-05-24 14:35:36 -0500

    Initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar.js';
+import SearchResults from '../SearchResults/SearchResults.js';
+import Playlist from '../Playlist/Playlist.js';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {name: 'Song',
+        artist: 'Prince',
+        album: 'Musicology',
+        id: '1'}
+      ]
+  },
+  {
+    playlistName: 'Roadtrip'
+  },
+  {
+    playlistTracks: [
+      {name: 'Bye Bye Bye',
+      artist: 'NSync',
+      album: 'No Strings Attached',
+      id: '2'}
+    ]
+  }
+}
+
+  render() {
+    return (
+      <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+  <div className="App">
+    <SearchBar/>
+    <div className="App-playlist">
+      <SearchResults searchResults ={this.state.searchResults}/>
+      <Playlist playlistName ={this.state.playlistName} playlistTracks ={this.state.playlistTracks}/>
+    </div>
+  </div>
+</div>
+    );
+  }
+}
+
+export default App;
